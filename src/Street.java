47cc6bc8d0341b721111be5d0a34a7b60962816d
lucsailor <seemann.luca@gmail.com>
2024-02-21 16:16:42 +0100

    bittte

--- src/Street.java
+++ src/Street.java
@@ -1,15 +1,19 @@
 public class Street {
     String name;
-    int[] price;
+    int price;
+    int[] rent;
     int[] group;
     int upgrade_cost;
+    String type;
     Player owner;
     static int i = 0;
 
-    public Street (String name, int[] price, int upgrade_cost){
+    public Street (String name, int[] rent, int upgrade_cost, int price, String type, int[] group){
         this.name = name;
-        this.price = price;
+        this.rent = rent;
         this.upgrade_cost = upgrade_cost;
+        this.price = price;
+        this.type = type;
         Player.Streets[i] = this;
         i++;
 
