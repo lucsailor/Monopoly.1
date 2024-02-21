9fc339ec027e894b0ddb512ed1d1c9d859adf6f3
lucsailor <seemann.luca@gmail.com>
2024-02-21 15:14:07 +0100

    bittte

---
+++ src/Player.java
@@ -0,0 +1,6 @@
+public class Player {
+    public String name;
+    public int Cash;
+    public static Street[] Streets;
+
+}
---
+++ src/Street.java
@@ -0,0 +1,17 @@
+public class Street {
+    String name;
+    int[] price;
+    int[] group;
+    int upgrade_cost;
+    Player owner;
+    static int i = 0;
+
+    public Street (String name, int[] price, int upgrade_cost){
+        this.name = name;
+        this.price = price;
+        this.upgrade_cost = upgrade_cost;
+        Player.Streets[i] = this;
+        i++;
+
+    }
+}
